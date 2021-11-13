bc6b084a2af8aa38a40b8c2b86c53087702f4de6
Selma Kahvedžić <80279038+selmakahvedzic@users.noreply.github.com>
2021-11-13 15:38:35 +0100

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -1,216 +1,38 @@
 #include <iostream>
-#include <stdexcept>
-template <typename Tip>
+using namespace std;
 
-
-class Red
-{
-    struct Cvor {
-        Tip element;
-        Cvor *sljedeci;
-    };
-
-    Cvor * pocetakReda;
-    Cvor *krajReda;
-    int duzina;
-
-public:
-
-    Red():pocetakReda(nullptr), krajReda(nullptr), duzina(0) {}
-    Red(const Red<Tip> &r)
-    {
-        duzina=r.duzina;
-        Cvor *p(r.pocetakReda);
-        pocetakReda=nullptr;
-        krajReda=nullptr;
-
-        while(p!=0) {
-            Cvor *novi=new Cvor;
-            novi->sljedeci=nullptr;
-            novi->element=p->element;
-            if(pocetakReda==nullptr) {
-                pocetakReda=novi;
-                krajReda=novi;
-            } else krajReda->sljedeci=novi;
-            p=p->sljedeci;
-            krajReda=novi;
-        }
-    }
-
-    ~Red()
-    {
-        while(duzina!=0) {
-
-            Cvor *pomocni=pocetakReda;
-            pocetakReda=pocetakReda->sljedeci;
-            delete pomocni;
-            duzina--;
-        }
-
-        pocetakReda=nullptr;
-        krajReda=nullptr;
-    }
-
-    Red &operator=(const Red &r)
-    {
-        if(this==&r) return *this;
-        while(duzina!=0) {
-            Cvor *pomocni=pocetakReda;
-            pocetakReda=pocetakReda->sljedeci;
-            delete pomocni;
-            duzina--;
-        }
-        duzina=r.duzina;
-        Cvor *p(r.pocetakReda);
-        pocetakReda=nullptr;
-        krajReda=nullptr;
-        while(p!=0) {
-            Cvor *novi=new Cvor;
-            novi->sljedeci=nullptr;
-            novi->element=p->element;
-            if(pocetakReda==nullptr) {
-               pocetakReda=novi;
-                krajReda=novi;
-            } else krajReda->sljedeci=novi;
-            p=p->sljedeci;
-            krajReda=novi;
-        }
-        return *this;
-    }
-
- 
-    Tip skini()
-    {
-        if(duzina==0) throw("Izutetak");
-        Tip x=pocetakReda->element;
-        Cvor *pomocni=pocetakReda;
-        if(duzina==1) {
-            pocetakReda=nullptr;
-            krajReda=nullptr;
-            delete pomocni;
-        } else {
-            pocetakReda=pocetakReda->sljedeci;
-            pomocni->sljedeci=nullptr;
-            delete pomocni;
-            pomocni=pocetakReda;
-        }
-        duzina--;
-        return x;
-    }
-
-    void stavi(const Tip &el)
-    {
-        if(duzina==0) {
-            Cvor *novi=new Cvor;
-            novi->element=el;
-            pocetakReda=novi;
-            krajReda=novi;
-        } else {
-            Cvor *novi=new Cvor;
-            novi->element=el;
-            krajReda->sljedeci=novi;
-            krajReda=novi;
-            krajReda->sljedeci=nullptr;
-        }
-        duzina++;
-    }
-
-   void brisi()
-    {
-        Cvor *pomocni(pocetakReda);
-        while(pocetakReda!=nullptr) {
-            pocetakReda=pocetakReda->sljedeci;
-            delete pomocni;
-            pomocni=pocetakReda;
-            duzina--;
-        }
-        duzina=0;
-        pocetakReda=nullptr;
-        krajReda=nullptr;
-    }
+int fib_petlja(int n) {
+    int pretprosli(0), prosli(1), rezultat;
     
-   Tip &celo()
-    {
-        if(duzina==0) throw("Red je prazan");
-        return pocetakReda->element;
+    for(int i=2; i<=n; i++) {
+        rezultat=pretprosli+prosli;
+       pretprosli=prosli;
+        prosli=rezultat;
     }
-
-    int brojElemenata()
-    {
-        return duzina;
-    }
-};
-
-void TestBrojElemenata(Red<int>r)
-{
-    int n(r.brojElemenata());
-    for(int i=0; i<n; i++) {
-        std::cout << r.brojElemenata()<<", "<<r.skini();
-    }
-    std::cout << r.brojElemenata();
+    return rezultat;
 }
 
-bool TestBrisi(Red<int>&r)
-{
-    r.brisi();
-    if(r.brojElemenata()==0) return true;
-    return false;
+int fib2_0(int n, int pretprosli=0, int prosli=1, int rez=0) {
+    if(n==0) return 0; 
+    else if(n==1) return prosli;
+    rez=prosli+pretprosli;
+    return fib2_0(n-1,prosli,rez);
 }
 
-void TestSkini(Red<int> &r)
-{
-    int n(r.brojElemenata());
-    for(int i=0; i<n; i++) {
-        std::cout << r.skini() <<" ";
-    }
+int nzd(int x, int y) {
+    if(y==0) return x; 
+    return nzd(y, x%y);
 }
 
-void TestStavi(Red<int>&r)
-{
-    for(int i=0; i<13; i++) {
-        r.stavi(2*i+15);
-    }
+void Test1() {
+    std::cout << fib2_0(20);
 }
 
-void TestCelo(Red<int>&r)
-{
-    int n(r.brojElemenata());
-    for(int i=0; i<n; i++) {
-        r.stavi(i+15);
-        std::cout<<r.celo()<<" ";
-    }
+void Test2() {
+    std::cout <<nzd(6,561);
 }
 
-
-bool Test1()
-{
-    Red<int>r;
-    for(int i=7; i<5; i++) r.stavi(i);
-    r.brisi();
-    if(r.brojElemenata()==0) return true;
-    return false;
-
-}
-
-bool Test2()
-{
-    Red<int>r;
-    for(int i=6; i>=3; i--)
-        r.stavi(i);
-    if(r.celo()==6) return true;
-    return false;
-
-}
-
-bool Test3()
-{
-    Red<int>r;
-    if(r.brojElemenata()==0) return true;
-    return false;
-}
-
-int main()
-{
-    std::cout << "Pripremna zadaća za vježbu 4, Zadatak 1";
+int main() {
+    std::cout << "Pripremna Zadaca Za Vjezbu 5, Zadatak 1";
     return 0;
 }
